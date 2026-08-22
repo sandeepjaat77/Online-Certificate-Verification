5df41b9e1dca45640af0a6440efa08de6f946979
sandeepjaat77 <sandeep774086@gmail.com>
2026-08-22 22:26:47 +0530

    Refactor connectDB to use async/await
    Refactor connectDB function to use async/await for better error handling and to exit process on failure.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
-const connectDB = () => {
-    mongoose
-        .connect(process.env.MONGO_URI)
-        .then(() => {
-            console.log("MongoDB Connected Successfully");
-        })
-        .catch((error) => {
-            console.log("MongoDB Connection Failed");
-            console.log(error.message);
-        });
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+
+        console.log("MongoDB Connected Successfully");
+    } catch (error) {
+        console.log("MongoDB Connection Failed");
+        console.log(error.message);
+
+        process.exit(1);
+    }
 };
 
 module.exports = connectDB;
